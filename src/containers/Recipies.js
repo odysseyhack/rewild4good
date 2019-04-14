5793e2c710fdf54f32f87bddec3ea0c255b52404
Madhusudhan Srinivasa <me@madhums.me>
2019-04-14 09:40:03 +0200

    add details

--- src/containers/Recipies.js
+++ src/containers/Recipies.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import qs from 'qs';
+import classnames from 'classnames';
 import {
   ListGroup,
   ListGroupItem,
@@ -9,34 +11,81 @@ import {
 const data = [
   {
     name: 'Reforestation x',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled...',
     verified: true,
     increase_jobs: '24%',
-    duration: 293
+    decreases_flooding: '10%',
+    duration: '165d'
   },
   {
     name: 'Reforestation y',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled...',
     verified: true,
     increase_jobs: '34%',
-    duration: 222
+    decreases_flooding: '23%',
+    duration: '302d',
+    optimal: true
   },
   {
     name: 'Reforestation z',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled...',
     verified: false,
     increase_jobs: '12%',
-    duration: 994
+    decreases_flooding: '-2%',
+    duration: '160d'
   }
 ];
 
 export default class Recipies extends Component {
+  state = {
+    tooltips: {}
+  };
+
+  toggle = e => {
+    this.setState({
+      tooltips: {
+        [e.target.id]: !this.state.tooltips[e.target.id],
+        ...this.state.tooltips
+      }
+    });
+  };
+
   render() {
+    const query = qs.stringify(window.location.search.slice(1));
     return (
       <div className="container my-4">
         <h2 className="mb-4">Recipies</h2>
         <ListGroup>
           {data.map((item, index) => (
-            <ListGroupItem key={index}>
+            <ListGroupItem key={index} color={item.optimal && 'success'}>
               <ListGroupItemHeading>{item.name}</ListGroupItemHeading>
               <ListGroupItemText>{item.description}</ListGroupItemText>
+              <div className="text-muted">
+                Jobs{' '}
+                <span
+                  className={classnames({
+                    'text-danger': item.increase_jobs.includes('-'),
+                    'text-success': !item.increase_jobs.includes('-')
+                  })}>
+                  {item.increase_jobs}
+                </span>
+                &nbsp;&nbsp; Duration {item.duration}
+                &nbsp;&nbsp; Flooding{' '}
+                <span
+                  className={classnames({
+                    'text-danger': item.decreases_flooding.includes('-'),
+                    'text-success': !item.decreases_flooding.includes('-')
+                  })}>
+                  {item.decreases_flooding}
+                </span>
+                <br />
+                <div className="small">
+                  <em>{JSON.stringify(query)}</em>
+                </div>
+              </div>
             </ListGroupItem>
           ))}
         </ListGroup>
